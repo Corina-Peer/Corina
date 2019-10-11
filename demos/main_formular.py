5fadd0f55dcd3c2f24f26176c0a47b0fbf70135a
Corina-Peer <55581676+Corina-Peer@users.noreply.github.com>
2019-10-11 15:10:18 +0200

    1. Versuch das Flussdiagramm hochzuladen

--- demos/main_formular.py
+++ demos/main_formular.py
@@ -2,3 +2,6 @@
     <input type="text" name="vorname" placeholder="Bitte geben Sie Ihren Namen ein">
     <button type="submit">Senden</button>
 </form>
+
+
+CTRL + Shift + P = damit können Zeilen sortiert werden 
